7a755ea4b26e13609b20ae2143e0328e421e14df
Francesco Calò <fcaloisme@gmail.com>
2021-08-16 22:36:09 +0200

    Refining webpack config

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,11 +1,28 @@
 import path from "path"
-import RemarkHTML from "remark-html"
 import { URL } from "url"
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const url = new URL(import.meta.url)
 const dir = path.dirname(url.pathname)
 
-console.log('dir is', dir)
+class Stripper {
+  apply (compiler) {
+    compiler.hooks.compilation.tap('Stripper', (compilation) => {
+      console.log('Stripper going at it')
+
+      // Static Plugin interface |compilation |HOOK NAME | register listener
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+        'Stripper', // <-- Set a meaningful name here for stacktraces
+        (data, cb) => {
+          // Manipulate the content
+          data.html = data.html.replace('<script type="module" src="src/index.js"></script>', '')
+          cb(null, data)
+        }
+      )
+    })
+  }
+}
+
 const config = {
   mode: "none",
   entry: "./docs-src/src/index.js",
@@ -13,28 +30,19 @@ const config = {
     filename: "main.js",
     path: path.resolve(dir, "docs")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "body",
+      template:  path.resolve(dir, "docs-src/index.html")
+    }),
+    new Stripper()
+  ],
   module: {
     rules: [
       {
         test: /\.css/,
         use: ["style-loader", "css-loader"]
       },
-      {
-        test: /\.md/,
-        use: [
-          {
-            loader: "html-loader"
-          },
-          {
-            loader: "remark-loader",
-            options: {
-              remarkOptions: {
-                plugins: [RemarkHTML]
-              }
-            }
-          }
-        ]
-      }
     ]
   }
 }
